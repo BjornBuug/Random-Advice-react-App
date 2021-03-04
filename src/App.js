a57e376dbb86592822d8b25af9cf4648f5cde4f4
tayarthouail <tayarthouail@gmail.com>
2021-03-04 11:06:40 +0100

    final version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const [advices , setAdvices] = useState({});
+const [isLoading, setIsLoading]= useState(true);
+
+//Destructuring advice from response.data.slip.advice with const {advice} = response.data.slip;
+useEffect(()=> {
+    const getFetchAdvice = async () => {
+      const result = await fetchAdvice();
+      setAdvices(result);
+      setIsLoading(false)
+
+    }
+    getFetchAdvice();
+    }
+    
+,[])
+
+const fetchAdvice = async () => {
+  const response = await axios.get('https://api.adviceslip.com/advice')
+  const {advice} = response.data.slip;
+  setAdvices(advice);
+  return advice;
+  
+}
+  
+  return isLoading ?
+   ( 'is Loading...') :
+     (<div className="App">
+    <div className="card">
+        <h3 className="heading">{advices}</h3>
+        <button className="button" onClick={fetchAdvice}> Advice me !</button>
+    </div>
     </div>
   );
-}
 
+}
 export default App;
